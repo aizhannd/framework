3c73c65d067feb6b03fa98cd0ca056f3e4de6757
Aizhan Dauylbayeva <Aizhan_Dauylbayeva@epam.com>
2018-12-14 15:05:09 +0600

    fixed isUserSignedIn() in BaseTest

--- src/main/java/tests/BaseTest.java
+++ src/main/java/tests/BaseTest.java
@@ -5,8 +5,6 @@ import framework.common.pages.HomePage;
 import framework.common.pages.InboxPage;
 import framework.core.driver.Driver;
 import framework.core.util.Waiting;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -15,18 +13,14 @@ public class BaseTest {
     protected InboxPage inbox;
     protected User user = new User();
 
-    @FindBy(xpath = "//*[@id='PH_user-email']")
-    private WebElement userEmailIdentificator;
-
     @BeforeClass
     public void login() {
         inbox = new HomePage().open().fillUsername(user.getUsername()).fillPassword(user.getPass()).signIn();
         Waiting.waitForPageIsLoaded(25000);
-        Waiting.waitForElementVisibleEnabled(userEmailIdentificator);
     }
 
     public boolean isUserSignedIn() {
-        return userEmailIdentificator.getText().contains(user.getUsername());
+        return inbox.getUserName().equals(user.getUsername());
     }
 
     @AfterClass(description = "closePanel browser")
