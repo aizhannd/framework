ddc043d4b02feb9b7aefaae4ed8180e1d430df54
Aizhan Dauylbayeva <Aizhan_Dauylbayeva@epam.com>
2019-01-11 17:11:25 +0600

    MyLogger.LOGGER.info in HomePage class

--- src/main/java/framework/common/pages/HomePage.java
+++ src/main/java/framework/common/pages/HomePage.java
@@ -19,26 +19,26 @@ public class HomePage extends BasePage {
 
     public HomePage open() {
         driver.get("https://mail.ru");
-        MyLogger.info("Going to URL: " + "https://mail.ru");
+        MyLogger.LOGGER.info("Going to URL: " + "https://mail.ru");
         Waiting.waitForElementEnabled(username);
         return this;
     }
 
     public HomePage fillUsername(String login) {
         username.sendKeys(login);
-        MyLogger.info("The username filling");
+        MyLogger.LOGGER.info("The username filling");
         return this;
     }
 
     public HomePage fillPassword(String pass) {
         password.sendKeys(pass);
-        MyLogger.info("Password filling");
+        MyLogger.LOGGER.info("Password filling");
         return this;
     }
 
     public InboxPage signIn() {
         signInButton.click();
-        MyLogger.info("The signIn button clicking");
+        MyLogger.LOGGER.info("The signIn button clicking");
         return new InboxPage();
     }
 }
