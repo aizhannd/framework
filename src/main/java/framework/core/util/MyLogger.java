318dbe7a49bb0efbde1266233235d61bd9a38ec9
Aizhan Dauylbayeva <Aizhan_Dauylbayeva@epam.com>
2019-01-11 16:46:28 +0600

    MyLogger class modified

--- src/main/java/framework/core/util/MyLogger.java
+++ src/main/java/framework/core/util/MyLogger.java
@@ -1,30 +1,26 @@
 package framework.core.util;
-import org.apache.log4j.Logger;
+import rp.com.google.common.io.BaseEncoding;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 public class MyLogger {
-    public static Logger logger = Logger.getLogger(MyLogger.class);
 
-    public static void error(String message) {
-        logger.error(message);
+    public static final Logger LOGGER = LogManager.getLogger(MyLogger.class);
+
+    private MyLogger(){
     }
 
-    public static void error(String message, Throwable throwable) {
-        logger.error(message, throwable);
+    public static void log(File file, String message) {
+        LOGGER.info("RP_MESSAGE#FILE#{}#{}", file.getAbsolutePath(), message);
     }
 
-    public static void info(String message) {
-        logger.info(message);
+    public static void log(byte[] bytes, String message) {
+        LOGGER.info("RP_MESSAGE#BASE64#{}#{}", BaseEncoding.base64().encode(bytes), message);
     }
 
-    public static void debug(String message) {
-        logger.debug(message);
-    }
-
-    public static void warn(String message) {
-        logger.warn(message);
-    }
-
-    public static void trace(String message) {
-        logger.trace(message);
+    public static void logBase64(String base64, String message) {
+        LOGGER.info("RP_MESSAGE#BASE64#{}#{}", base64, message);
     }
 }
