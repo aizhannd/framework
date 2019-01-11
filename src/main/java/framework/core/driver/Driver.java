42e118cae1b15973f2b82de52b98745368f1e012
Aizhan Dauylbayeva <Aizhan_Dauylbayeva@epam.com>
2019-01-11 16:56:41 +0600

    changed Driver

--- src/main/java/framework/core/driver/Driver.java
+++ src/main/java/framework/core/driver/Driver.java
@@ -1,24 +1,29 @@
 package framework.core.driver;
 
+import framework.core.util.MyLogger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class Driver {
-    private static WebDriver driver;
-
-    private Driver() {
-    }
+    private static WebDriver instance;
 
     public static WebDriver getWebDriverInstance() {
-        if (driver == null) {
-            WebDriverCreator creator = new ChromeDriverCreator();
-            driver = creator.FactoryMethod();
-            driver = new DriverDecorator();
-            driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-            driver.manage().window().maximize();
+        if (instance != null) {
+            return instance;
         }
+        return instance = init();
+    }
+
+    private static WebDriver init() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/driverbinaries/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        MyLogger.info("New driver instantiated");
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        MyLogger.info("Implicit wait applied on the driver for 10 seconds");
+        driver.manage().window().maximize();
         return driver;
     }
 }
